1be9b0d1473a3803896ac6693affef176a289299
Danilo Salaz <danilo_salaz@hotmail.es>
2014-10-20 21:35:12 -0500

    Registro de Usuario

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+	factory :user do
+		name    "Danilo Salaz"
+		email  "danilo_salaz@hotmail.es"
+		password   "danilo123"
+		password_confirmation  "danilo123"
+	end
+end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,11 +1,41 @@
 require 'spec_helper'
 
 describe "UserPages" do
+	
 	subject { page }
+	describe "pagina de perfil" do
+		let(:user) { FactoryGirl.create(:user) }
+		before { visit user_path(user) }
+
+		it { should have_content(user.name) }
+		it { should have_title(user.name) }
+	end	
 	describe "Pagina de registro" do
 		before { visit registro_path }
+
 		it { should have_content('Registro') }
 		it { should have_title(full_title('Registro')) }
 	end
+	decribe "registro" do
+		before { visit registro_path }
+		let(:submit) { "Crear mi cuenta" }
 
+		describe "con informacion invalida" do
+			it "no decribe crear al usuario" do
+				expect {click_botton submit }.not_to change(User, :cont)
+			end
+		end
+		describe "con informacion valida" do
+			before do
+				fill_in "Name",      	whith: "Usuario Ejemplo"
+				fill_in "Email", 		whith: "User@ejemplo.com"
+				fill_in "Password", 	whith: "password"
+				fill_in "Confirmacion", whith: "password"
+			end	
+			it "debe crear un usuario" do
+				expect { click_botton submit }.to change(User, :count).by(1)
+			end
+		end			
+
+	end
 end
